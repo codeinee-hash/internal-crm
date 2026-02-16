d299abf03259f19db9b669d97dd7422f8c09f83e
codeinee <>
2026-02-17 02:36:47 +0600

    init project with shadcn and tailwind

---
+++ src/app/app.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/shared/ui/button'
+
+export function App() {
+	return (
+		<div className='text-2xl font-bold text-teal-400'>
+			App
+			<Button>Button</Button>
+		</div>
+	)
+}
